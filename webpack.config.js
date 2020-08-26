844fc4f42de6fc7973d8f7ea2214556623d16725
dylan <dylan.riley@nice.org.uk>
2020-08-26 16:03:58 +0100

    CB-6 Create example page

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
@@ -21,5 +22,13 @@ module.exports = {
 	output: {
 		filename: "cookie-banner.js",
 		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [new HtmlWebpackPlugin()],
+	devServer: {
+		port: process.env.PORT || 8089,
+		host: "localhost",
+		publicPath: "/",
+		contentBase: path.join(__dirname, "example/"),
+		open: true,
 	},
 };
