2c5180fba4af95d21a8afce47c1ff0beffe1464b
dylan <dylan.riley@nice.org.uk>
2020-09-03 10:44:39 +0100

    CB-33 Edit jest.config.js for teamcity reporting

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
 	transform: {
 		"^.+\\.(ts)$": "ts-jest",
 	},
+	preset: "ts-jest",
+	collectCoverage: process.env.TEAMCITY_VERSION ? true : false,
+	collectCoverageFrom: [
+		"src/**/*.{js,jsx,ts,tsx}",
+		"!**/node_modules/**",
+		"!src/graphql-fragments/**",
+	],
+	testResultsProcessor: "jest-teamcity-reporter",
 };
