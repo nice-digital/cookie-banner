00d2679cebdbd08a61bdc300b291218f8fd620ec
Ian Routledge <ian.routledge@nice.org.uk>
2020-10-22 16:19:18 +0100

    CB-38 Reinstate cookie parsing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,12 @@
+import Cookie from "js-cookie";
+
 import { loadCookieControl } from "./index";
 import { cookieControlConfig } from "./cookie-control-config";
 import { CookieControl } from "./cookie-control";
+
+jest.mock("js-cookie", () => ({
+	getJSON: jest.fn(),
+}));
 
 describe("Load cookie control tests", () => {
 	beforeEach(() => {
@@ -18,5 +24,51 @@ describe("Load cookie control tests", () => {
 		expect(window.dataLayer).toBeFalsy();
 		loadCookieControl();
 		expect(window.dataLayer).toBeTruthy();
+	});
+
+	it("should set CookieControl properties to false with missing cookie", () => {
+		loadCookieControl();
+
+		expect(CookieControl).toMatchObject({
+			analyticsCookies: false,
+			preferenceCookies: false,
+			marketingCookies: false,
+		});
+	});
+
+	it("should CookieControl object properties from existing cookie with revoked consent", () => {
+		((Cookie.getJSON as unknown) as jest.Mock).mockImplementation(() => ({
+			optionalCookies: {
+				analytics: "revoked",
+				preferences: "revoked",
+				marketing: "revoked",
+			},
+		}));
+
+		loadCookieControl();
+
+		expect(CookieControl).toMatchObject({
+			analyticsCookies: false,
+			preferenceCookies: false,
+			marketingCookies: false,
+		});
+	});
+
+	it("should CookieControl object properties from existing cookie with consent", () => {
+		((Cookie.getJSON as unknown) as jest.Mock).mockImplementation(() => ({
+			optionalCookies: {
+				analytics: "accepted",
+				preferences: "accepted",
+				marketing: "accepted",
+			},
+		}));
+
+		loadCookieControl();
+
+		expect(CookieControl).toMatchObject({
+			analyticsCookies: true,
+			preferenceCookies: true,
+			marketingCookies: true,
+		});
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
+import Cookies from "js-cookie";
 import { CookieControl } from "./cookie-control";
 
 import { cookieControlConfig } from "./cookie-control-config";
 import "./cookie-control.scss";
+import { CookieControlCookie } from "./types/cookie-control";
+
+// Ensure the GTM dataLayer array exists, in case the cookie banner is loaded before GTM
+const ensureDataLayer = () => {
+	window.dataLayer = window.dataLayer || [];
+};
+
+// Parse the cookie that gets set by Cookie Control ourselves.
+// This allows the properties like CookieControl.preferenceCookies to be available
+// even before the license has loaded asynchronously.
+const parseCookieControlCookie = () => {
+	const cookieControlCookie = Cookies.getJSON(
+		"CookieControl"
+	) as CookieControlCookie;
+
+	const analyticsCookies =
+			cookieControlCookie?.optionalCookies.analytics === "accepted",
+		preferenceCookies =
+			cookieControlCookie?.optionalCookies.preferences === "accepted",
+		marketingCookies =
+			cookieControlCookie?.optionalCookies.marketing === "accepted";
+
+	CookieControl.analyticsCookies = analyticsCookies;
+	CookieControl.preferenceCookies = preferenceCookies;
+	CookieControl.marketingCookies = marketingCookies;
+};
 
 export const loadCookieControl = (): void => {
-	// Ensure the GTM dataLayer array exists, in case the cookie banner is loaded before GTM
-	window.dataLayer = window.dataLayer || [];
+	ensureDataLayer();
+
+	parseCookieControlCookie();
 
 	CookieControl.load(cookieControlConfig);
 };
