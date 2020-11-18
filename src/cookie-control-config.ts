a4acc85b7e0fb0bae2345382d71810d38d89e4a5
Andy Child <48481960+AndyChildNice@users.noreply.github.com>
2020-11-18 15:39:38 +0000

    CB-64 DocSupply Cookies (#28)
    * CB-64 adds DocSupply cookies
    
    * CB-64 fix snapshot test

--- src/cookie-control-config.ts
+++ src/cookie-control-config.ts
@@ -74,6 +74,9 @@ export const cookieControlConfig: CookieControlConfig = {
 		"evercookie_png",
 		".ukps",
 		".ukpsLease",
+		//DocSupply
+		"sticky-session",
+		".AspNetCore.Cookies",
 	],
 	optionalCookies: [
 		{
