38f1d864db8b289acb76609884017a4d6fa99185
Grendelsson <Grendelsson@users.noreply.github.com>
2020-12-22 09:23:58 +0000

    Update cookie-control-config.ts
    Add Google Optimize cookies and remove VWO cookies from 'analytics' group allow-list

--- src/cookie-control-config.ts
+++ src/cookie-control-config.ts
@@ -127,9 +127,9 @@ export const cookieControlConfig: CookieControlConfig = {
 				// Hotjar
 				"_hj*",
 				"ajs_*",
-				// VWO
-				"_vwo*",
-				"_vis*",
+				// Google Optimize
+				"_gaexp",
+				"_opt_*",
 			],
 			onAccept: function (): void {
 				CookieControl.analyticsCookies = true;
