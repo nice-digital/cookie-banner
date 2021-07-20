b918f1ad76a27d3ce4d94c5e551d03e6514c405c
Grendelsson <Grendelsson@users.noreply.github.com>
2021-07-20 10:12:39 +0100

    CB-76 Update cookie banner to allow the new ga4 cookie (#37)
    * CB-76-Update cookie banner
    
    Update cookie banner to allowlist the new GA4 cookie
    
    * Update statement date
    
    * Update cookie-control-config.test.ts.snap
    
    * CB-76 Update deps
    
    * CB-76 Upgrade to webpack 5
    
    * CB-76 Cookie name match expression changed
    
    Co-authored-by: Ian Routledge <ian@ediblecode.com>
    Co-authored-by: Warren <w@rren.dev>

--- src/cookie-control-config.ts
+++ src/cookie-control-config.ts
@@ -129,6 +129,7 @@ export const cookieControlConfig: CookieControlConfig = {
 				"_ga",
 				"_gat_UA-*",
 				"_gid",
+				"_ga_*",
 				// Hotjar
 				"_hj*",
 				"ajs_*",
@@ -291,7 +292,7 @@ export const cookieControlConfig: CookieControlConfig = {
 		description: "For more information, view our",
 		name: "cookie statement.",
 		url: "https://www.nice.org.uk/cookies",
-		updated: "15/03/2021",
+		updated: "09/07/2021",
 	},
 
 	// Branding
